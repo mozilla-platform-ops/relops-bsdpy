bfdad867f1edb3a70d5c33956d6d74bd5bb7932b
Jake Watkins <jwatkins@mozilla.com>
2018-08-22 17:04:46 -0700

    Use env to find python

--- bsdpserver.py
+++ bsdpserver.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 ################################################################################
 # Copyright 2015 The Regents of the University of Michigan
 #
